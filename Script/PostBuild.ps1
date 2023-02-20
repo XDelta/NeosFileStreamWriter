aa9d189616e96839b43139c5025abab440388b43
Delta <delta@deltawolf.net>
2023-02-20 00:26:44 -0800

    Moved PostBuild.ps1 Scripts/

--- Script/PostBuild.ps1
+++
@@ -1,3 +0,0 @@
-Add-Type -Path "$($args[0])\Neos_Data\Managed\PostX.dll"
-Set-Location  ".\bin\$($args[1])"
-[PostX.NeosAssemblyPostProcessor].GetMethod("Process").Invoke($null, @("$PWD\NeosFileStreamWriter.dll", "$PWD"))
