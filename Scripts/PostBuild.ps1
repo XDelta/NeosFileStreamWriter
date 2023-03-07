910d3c291a7b3e06b546fc0e8c792318fb938bf9
Delta <delta@deltawolf.net>
2023-03-06 22:07:06 -0800

    Change fallback NeosVR path and added some comments

--- Scripts/PostBuild.ps1
+++ Scripts/PostBuild.ps1
@@ -1,16 +1,16 @@
 #If Args are not provided, set them to defaults
-$NeosPath = if ($args[0] -ne $null) { $args[0] } else {"R:\SteamLibrary\steamapps\common\NeosVRBetaBuilds\NeosStandalone\app"}
-$ConfigurationName = if ($args[1] -ne $null) { $args[1] } else {"AutoPostX"}
+$NeosPath = if ($args[0] -ne $null) { $args[0] } else {"C:\Program Files (x86)\Steam\steamapps\common\NeosVR"} #This is a fallback path incase one is not provided as an arg, ie by directly running this script
+$ConfigurationName = if ($args[1] -ne $null) { $args[1] } else {"AutoPostX"} #Build output path under \bin\
 #If run from the Scripts folder directly, go up one folder to the project directory
-if ($PWD.Path.EndsWith("Scripts")){
+if ($PWD.Path.EndsWith("Scripts")) {
     $ProjectPath = Split-Path $PWD.Path -Parent
 } else {
     $ProjectPath = $PWD.Path
 }
 
+#Verify output location exists
 $OutputLocation = "$($ProjectPath)\bin\$($ConfigurationName)\"
-if (-not (Test-Path "$OutputLocation"))
-{
+if (-not (Test-Path "$OutputLocation")) {
     Write-Error "Output Location path '$OutputLocation' does not exist"
     Exit 2
 }
@@ -22,4 +22,5 @@ Copy-Item -Path "$($NeosPath)\Neos_Data\Managed\FrooxEngine.dll" -Destination $O
 Copy-Item -Path "$($NeosPath)\BaseX.dll" -Destination $OutputLocation -ErrorAction Continue
 Copy-Item -Path "$($NeosPath)\Neos_Data\Managed\System.Threading.Tasks.Dataflow.dll" -Destination $OutputLocation -ErrorAction Continue
 
+#Run PostX Process
 [PostX.NeosAssemblyPostProcessor].GetMethod("Process").Invoke($null, @("$OutputLocation\NeosFileStreamWriter.dll", "$OutputLocation"))
